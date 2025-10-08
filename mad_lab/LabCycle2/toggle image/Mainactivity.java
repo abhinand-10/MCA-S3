766d839fc7c1b0f7f2d8292e43e0cdc2fc7bb8b1
abhinand-10 <chn24mc003@ceconline.edu>
2025-10-08 11:04:17 +0530

    Create Mainactivity.java

---
+++ mad_lab/LabCycle2/toggle image/Mainactivity.java
@@ -0,0 +1,37 @@
+package com.example.toggle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    ImageView i1,i2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        i1=findViewById(R.id.img1);
+        i2=findViewById(R.id.img2);
+
+        i1.setOnClickListener(this);
+        i2.setOnClickListener(this);
+
+    }
+
+    public void onClick(View view){
+        if (view.getId()==R.id.img1){
+            i1.setVisibility(view.GONE);
+            i2.setVisibility(view.VISIBLE);
+        }
+        else{
+            i2.setVisibility(view.GONE);
+            i1.setVisibility(view.VISIBLE);
+        }
+    }
+
+
+}
