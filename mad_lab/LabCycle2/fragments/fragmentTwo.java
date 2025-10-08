a22e9c410a64f080527fc16bc5fa6ae7f780b5cf
abhinand-10 <chn24mc003@ceconline.edu>
2025-10-08 10:49:16 +0530

    Create fragmentTwo.java

---
+++ mad_lab/LabCycle2/fragments/fragmentTwo.java
@@ -0,0 +1,18 @@
+package com.example.fragment06;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+// This is the second fragment class.
+public class FragmentTwo extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+// Inflate the layout for this fragment from its XML file.
+        return inflater.inflate(R.layout.fragment_two, container, false);
+    }
+}
