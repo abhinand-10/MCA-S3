47b64d3c456756d82ae08a0424349df3cd2fa367
abhinand-10 <chn24mc003@ceconline.edu>
2025-10-08 11:10:27 +0530

    Create Second activity.java

---
+++ mad_lab/LabCycle 3/ExplicitIntent application/Second activity.java
@@ -0,0 +1,14 @@
+package com.example.explicitintentapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
